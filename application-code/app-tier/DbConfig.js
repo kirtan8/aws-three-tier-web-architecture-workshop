8d4e416adef675023fdd93075bda153bb3ac3ef8
kirtan8 <81466667+kirtan8@users.noreply.github.com>
2024-09-22 20:45:35 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : '10.0.1.72',
+    DB_USER : 'admin',
+    DB_PWD : 'admin1234',
+    DB_DATABASE : 'webappdb'
 });
